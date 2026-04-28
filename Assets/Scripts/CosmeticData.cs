ccec75f42cd2ddae8562a84273bc89c39a6f4b93
antonio <antoniorramirezmunoz6@gmail.com>
2026-04-28 14:10:43 +0200

    aa

--- Assets/Scripts/CosmeticData.cs
+++ Assets/Scripts/CosmeticData.cs
@@ -5,4 +5,9 @@ public class CosmeticData : ScriptableObject
 {
     public Sprite selectedSprite;
     public Color selectedColor;
+    public Sprite selectedLipShape;
+    
+    // NUEVAS VARIABLES PARA LA POSICIÓN Y ESCALA
+    public Vector3 lipPosition;
+    public Vector3 lipScale;
 }
