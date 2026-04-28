37fbd80fed44167d99ce226f94df4d78947a2544
AlexGreyrat <alexandrocaceresromero2004@gmail.com>
2026-04-28 13:48:14 +0200

    dada

--- Assets/Scripts/MakeUpItem.cs
+++ Assets/Scripts/MakeUpItem.cs
@@ -3,12 +3,4 @@ using UnityEngine;
 public class MakeupItem : MonoBehaviour
 {
     public int puntosQueDa = 10;
-    private void OnMouseDown()
-    {
-        GameManager.Instancia.SumarPuntos(puntosQueDa);
-
-        Debug.Log("¡Maquillaje recogido con un clic!");
-
-        Destroy(gameObject);
-    }
 }
